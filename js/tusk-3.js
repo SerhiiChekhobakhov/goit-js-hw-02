cafa85451765b34ba0f860029eba32fb4745267d
Serhii <113892131+SerhiiChekhobakhov@users.noreply.github.com>
2024-07-21 16:45:20 +0300

    Update tusk-3.js

--- js/tusk-3.js
+++ js/tusk-3.js
@@ -7,5 +7,5 @@ console.log(checkForSpam("JavaScript weekly newsletter"));
 console.log(checkForSpam("Get best sale offers now!"));
 console.log(checkForSpam("Amazing SalE, only tonight!"));
 console.log(checkForSpam("Trust me, this is not a spam message"));
-onsole.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
 console.log(checkForSpam("[SPAM] How to earn fast money?"));
